e78a2afd0f9be77c728eb70197224c57aa0e4477
zximeng <simonzhang8525@gmail.com>
2020-03-02 18:57:38 -0500

    added second file into the last one

--- rolling_control.py
+++ rolling_control.py
@@ -10,7 +10,42 @@ import os    # for OS calls
 import time
 import RPi.GPIO as GPIO
 # GPIO init
+
 GPIO.setmode(GPIO.BCM)
+GPIO.setup(12, GPIO.OUT)
+GPIO.setup(16, GPIO.OUT)
+chan_list = [17,22,23,27,19,26]
+
+p1 = GPIO.PWM(12, 46.5)  
+p2 = GPIO.PWM(16, 46.5)  
+p1.start(7)
+p2.start(7)
+while(True):
+	GPIO.setup(chan_list,GPIO.IN,pull_up_down = GPIO.PUD_UP)
+	time.sleep(0.1)
+	if(not GPIO.input(22)):
+		p1.ChangeFrequency(46.5)
+		p1.ChangeDutyCycle(7)
+	if(not GPIO.input(23)):
+		p1.ChangeFrequency(46.08)
+		p1.ChangeDutyCycle(7.83)		
+	if(not GPIO.input(17)):
+		p1.ChangeFrequency(46.95)
+		p1.ChangeDutyCycle(6.10)	
+	if(not GPIO.input(19)):
+		p2.ChangeFrequency(46.5)
+		p2.ChangeDutyCycle(7)
+	if(not GPIO.input(26)):
+		p2.ChangeFrequency(46.08)
+		p2.ChangeDutyCycle(7.83)		
+	if(not GPIO.input(27)):
+		p2.ChangeFrequency(46.95)
+		p2.ChangeDutyCycle(6.10)
+
+
+p1.stop()
+p2.stop()
+GPIO.cleanup()
 #pygame init 
 pygame.init()
 pygame.mouse.set_visible(False)
